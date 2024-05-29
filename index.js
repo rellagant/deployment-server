92bc4c7aa5088d33984d9d4fae01b775d81cc197
Barbara Csankova <barbara.cs@gmail.com>
2024-05-29 10:39:13 -0400

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5050;
 
+app.use(cors());
 app.use(express.json());
 
 const warehouseRoutes = require("./routes/warehouseRoute");
